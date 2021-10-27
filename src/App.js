663af07b3a72edd8da02510525223498e97e925f
Rajan Gurung <bardangrg0@gmail.com>
2021-10-27 11:44:32 +0545

    Added image Component in App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './Components/Home';
 import About from './Components/About'
 import Services from './Components/Services/ServicesElement'
 import Terminology from './Components/Terminology/TerminologyElement'
+import Video from './Components/Video/Video';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,6 +22,7 @@ function App() {
       <Home/>
       <About/>
       <Services/>
+      <Video/>
       <Terminology/>
       {/* 
       <Services/>
